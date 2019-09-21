d43977aff66b12d45eaed6c976ea618c6fd73626
nrs32 <38587739+nrs32@users.noreply.github.com>
2019-09-20 22:42:29 -0400

    add lab01 from PC

---
+++ lab01/app/src/main/java/edu/calvin/cs262/nrs32/MainActivity.java
@@ -0,0 +1,16 @@
+package edu.calvin.cs262.nrs32;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d("MainActivity", "Hello World");
+    }
+}
