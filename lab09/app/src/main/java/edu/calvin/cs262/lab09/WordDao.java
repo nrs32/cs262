42dafe627365a38bea4b65fd8868bc4b2b23f564
nrs32 <38587739+nrs32@users.noreply.github.com>
2019-11-01 15:55:29 -0400

    lab09 part1

---
+++ lab09/app/src/main/java/edu/calvin/cs262/lab09/WordDao.java
@@ -0,0 +1,21 @@
+package edu.calvin.cs262.lab09;
+
+import java.util.List;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+
+@Dao
+public interface WordDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    void insert(Word word);
+
+    @Query("DELETE FROM word_table")
+    void deleteAll();
+
+    @Query("SELECT * from word_table ORDER BY word ASC")
+    LiveData<List<Word>> getAllWords();
+}
